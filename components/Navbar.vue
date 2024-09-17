d74ad212ae1234824a266d9e955812efad9c8fdf
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-17 17:12:44 +0600

    feat: intial project setup done

---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="w-full fixed top-0 border-b">
+    <div
+      class="flex items-center justify-between w-full max-w-screen-lg mx-auto px-4 h-14"
+    >
+      <UAvatar
+        src="https://github.com/nuxt.png"
+        alt="Avatar"
+      />
+      <ThemeSwitcher />
+    </div>
+  </nav>
+</template>
