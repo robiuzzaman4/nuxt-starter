103c8c5857b2df197bd6b36bddacc408879fdaa0
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-26 18:37:56 +0600

    feat: add multiple color picker

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="w-full fixed top-0 border-b">
+  <nav class="w-full fixed top-0 border-b border-b-primary-200/75 dark:border-b-primary-900/50">
     <div
       class="flex items-center justify-between w-full max-w-screen-lg mx-auto px-4 h-14"
     >
@@ -7,7 +7,7 @@
         src="https://github.com/nuxt.png"
         alt="Avatar"
       />
-      <ThemeSwitcher />
+      <ColorPicker />
     </div>
   </nav>
 </template>
